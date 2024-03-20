bf9a9d1a9d2f888ecdf666b5aee7cbea51605c02
Mohamed Adel <72352419+Mohamed-Adel-A@users.noreply.github.com>
2024-03-20 17:53:03 +0200

    Update 100-evcar.js

--- 0x02-ES6_classes/100-evcar.js
+++ 0x02-ES6_classes/100-evcar.js
@@ -1,1 +1,13 @@
-#!/usr/bin/node
+import Car from './10-car.js';
+
+export default class EVCar extends Car {
+  constructor(brand, motor, color, range) {
+    super(brand, motor, color);
+    this._range = range;
+  }
+
+  cloneCar() {
+    const clonedCar = new Car(this._brand, this._motor, this._color);
+    return clonedCar;
+  }
+}
