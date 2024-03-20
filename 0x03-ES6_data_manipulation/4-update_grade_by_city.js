ba6062ab9b799b80cde9de3098c1acace7b0a052
Mohamed Adel <72352419+Mohamed-Adel-A@users.noreply.github.com>
2024-03-21 01:01:55 +0200

    Update 4-update_grade_by_city.js

--- 0x03-ES6_data_manipulation/4-update_grade_by_city.js
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -1,6 +1,6 @@
 function updateStudentGradeByCity(students, city, newGrades) {
-  return students.map(student => {
-    const foundGrade = newGrades.find(grade => grade.studentId === student.id);
+  return students.map((student) => {
+    const foundGrade = newGrades.find((grade) => grade.studentId === student.id);
     if (foundGrade) {
       return { ...student, grade: foundGrade.grade };
     } else {
