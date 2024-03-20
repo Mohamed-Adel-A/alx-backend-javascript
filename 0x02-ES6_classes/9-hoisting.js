242fdc182b61cbbdf1e5db68262adb3540c67d98
Mohamed Adel <72352419+Mohamed-Adel-A@users.noreply.github.com>
2024-03-20 17:52:29 +0200

    Update 9-hoisting.js

--- 0x02-ES6_classes/9-hoisting.js
+++ 0x02-ES6_classes/9-hoisting.js
@@ -1,1 +1,23 @@
-#!/usr/bin/node
+import HolbertonClass from './8-hbtn_class.js';
+
+class StudentHolberton {
+  constructor(firstName, lastName, holbertonClass) {
+    this._firstName = firstName;
+    this._lastName = lastName;
+    this._holbertonClass = holbertonClass;
+  }
+
+  get fullName() {
+    return `${this._firstName} ${this._lastName}`;
+  }
+
+  get holbertonClass() {
+    return this._holbertonClass;
+  }
+
+  get fullStudentDescription() {
+    return `${this._firstName} ${this._lastName} - ${this._holbertonClass.size} - ${this._holbertonClass.location}`;
+  }
+}
+
+export { HolbertonClass, StudentHolberton };
