eb7209364147acc387b87676e299a3336a03da28
Mohamed Adel <72352419+Mohamed-Adel-A@users.noreply.github.com>
2024-03-21 01:02:26 +0200

    Update 8-clean_set.js

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,5 +1,5 @@
 function cleanSet(set, startString) {
-  const filteredValues = [...set].filter(value => value.startsWith(startString));
+  const filteredValues = [...set].filter((value) => value.startsWith(startString));
   return filteredValues.join('-');
 }
 
