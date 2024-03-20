0363d664a431e258615b631b4a45ae62de5d1b6e
Mohamed Adel <72352419+Mohamed-Adel-A@users.noreply.github.com>
2024-03-20 18:05:56 +0200

    Update 2-hbtn_course.js

--- 0x02-ES6_classes/2-hbtn_course.js
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -1,8 +1,5 @@
 export default class HolbertonCourse {
   constructor(name, length, students) {
-    if (typeof name !== 'string') throw new TypeError('Name must be a string');
-    if (typeof length !== 'number') throw new TypeError('Length must be a number');
-
     this._name = name;
     this._length = length;
     this._students = students;
@@ -31,6 +28,12 @@ export default class HolbertonCourse {
   }
 
   set students(students) {
+    if (!(students instanceof Array)) {
+      throw new TypeError('Students must be an array of strings');
+    }
+    if (!students.every((student) => typeof student === 'string')) {
+      throw new TypeError('Students must be an array of strings');
+    }
     this._students = students;
   }
 }
