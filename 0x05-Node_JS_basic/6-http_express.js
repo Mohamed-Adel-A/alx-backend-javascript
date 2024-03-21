2d0b270f6049dbe8627467a1594f3f434ef89b3c
Mohamed Adel <72352419+Mohamed-Adel-A@users.noreply.github.com>
2024-03-21 14:41:34 +0200

    Update 6-http_express.js

--- 0x05-Node_JS_basic/6-http_express.js
+++ 0x05-Node_JS_basic/6-http_express.js
@@ -1,1 +1,13 @@
-#!/usr/bin/node
+const express = require('express');
+
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('Hello Holberton School!');
+});
+
+app.listen(1245, () => {
+  console.log('Server running at http://127.0.0.1:1245/');
+});
+
+module.exports = app;
