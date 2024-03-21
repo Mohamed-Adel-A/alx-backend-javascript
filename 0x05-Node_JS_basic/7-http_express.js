9cd737388aedbfa394644009087740a2235c7b14
Mohamed Adel <72352419+Mohamed-Adel-A@users.noreply.github.com>
2024-03-21 14:41:45 +0200

    Update 7-http_express.js

--- 0x05-Node_JS_basic/7-http_express.js
+++ 0x05-Node_JS_basic/7-http_express.js
@@ -1,1 +1,23 @@
-#!/usr/bin/node
+const express = require('express');
+const countStudents = require('./3-read_file_async');
+
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('Hello Holberton School!');
+});
+
+app.get('/students', async (req, res) => {
+  try {
+    await countStudents(process.argv[2]);
+    res.send();
+  } catch (error) {
+    res.status(500).send(error.message);
+  }
+});
+
+app.listen(1245, () => {
+  console.log('Server running at http://127.0.0.1:1245/');
+});
+
+module.exports = app;
