1ae15fb3d30067234ec68cfd276cc28b70816b18
Mohamed Adel <72352419+Mohamed-Adel-A@users.noreply.github.com>
2024-03-21 15:15:49 +0200

    Update 1-stdin.js

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,8 +1,11 @@
+process.stdin.setEncoding('utf8');
+
 process.stdout.write('Welcome to Holberton School, what is your name?\n');
+
 process.stdin.on('data', (data) => {
   process.stdout.write(`Your name is: ${data}`);
-  process.exit();
 });
-process.on('end', () => {
+
+process.stdin.on('end', () => {
   process.stdout.write('This important software is now closing\n');
 });
