8662119b9aba9a75871ef9f98f08eb6d9bbed6f8
Mohamed Adel <72352419+Mohamed-Adel-A@users.noreply.github.com>
2024-03-22 00:57:43 +0200

    Update 5-http.js

--- 0x05-Node_JS_basic/5-http.js
+++ 0x05-Node_JS_basic/5-http.js
@@ -1,7 +1,36 @@
 const http = require('http');
 const fs = require('fs');
 
-const countStudents = require('./3-read_file_async');
+function countStudents(path) {
+  return new Promise((resolve, reject) => {
+    fs.readFile(path, 'utf8', (err, data) => {
+      if (err) {
+        reject(new Error('Cannot load the database'));
+      } else {
+        const lines = data.split('\n').filter((line) => line !== '');
+        const students = lines.slice(1);
+        const studentsByField = {};
+        students.forEach((student) => {
+          const fields = student.split(',');
+          const field = fields[3];
+          if (!studentsByField[field]) {
+            studentsByField[field] = [];
+          }
+          studentsByField[field].push(fields[0]);
+        });
+        console.log(`Number of students: ${students.length}`);
+        for (const field in studentsByField) {
+          if (Object.hasOwnProperty.call(studentsByField, field)) {
+            const result = (`Number of students in ${field}: ${studentsByField[field].length}. List: ${studentsByField[field].join(', ')}`);
+          }
+        }
+        resolve(result);
+      }
+    });
+  });
+}
+
+
 
 const app = http.createServer(async (req, res) => {
   if (req.url === '/') {
