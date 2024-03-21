464e5c6da39b7b36981929bd4375b65e834b41f6
Mohamed Adel <72352419+Mohamed-Adel-A@users.noreply.github.com>
2024-03-21 14:41:24 +0200

    Update 5-http.js

--- 0x05-Node_JS_basic/5-http.js
+++ 0x05-Node_JS_basic/5-http.js
@@ -1,1 +1,24 @@
-#!/usr/bin/node
+const http = require('http');
+const countStudents = require('./3-read_file_async');
+
+const app = http.createServer(async (req, res) => {
+  if (req.url === '/') {
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
+    res.end('Hello Holberton School!\n');
+  } else if (req.url === '/students') {
+    try {
+      await countStudents(process.argv[2]);
+      res.writeHead(200, { 'Content-Type': 'text/plain' });
+      res.end();
+    } catch (error) {
+      res.writeHead(500, { 'Content-Type': 'text/plain' });
+      res.end(error.message);
+    }
+  }
+});
+
+app.listen(1245, () => {
+  console.log('Server running at http://127.0.0.1:1245/');
+});
+
+module.exports = app;
