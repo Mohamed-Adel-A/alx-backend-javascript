eec204a3fb47c9b5efe880fa3c9acfca798384fa
Mohamed Adel <72352419+Mohamed-Adel-A@users.noreply.github.com>
2024-03-21 14:42:24 +0200

    Update StudentsController.js

--- 0x05-Node_JS_basic/full_server/controllers/StudentsController.js
+++ 0x05-Node_JS_basic/full_server/controllers/StudentsController.js
@@ -1,1 +1,21 @@
-#!/usr/bin/node
+const readDatabase = require('../utils');
+
+class StudentsController {
+  static async getAllStudents(req, res) {
+    try {
+      const studentsByField = await readDatabase(process.argv[2]);
+      let response = 'This is the list of our students\n';
+      Object.entries(studentsByField).forEach(([field, students]) => {
+        response += `Number of students in ${field}: ${students.length}. List: ${students.join(', ')}\n`;
+      });
+      res.status(200).send(response);
+    } catch (error) {
+      res.status(500).send(error.message);
+    }
+  }
+
+  static async getAllStudentsByMajor(req, res) {
+    const { major } = req.params;
+    if (major !== 'CS' && major !== 'SWE') {
+      res.status(500).send('Major parameter must be CS or SWE');
+      return;
