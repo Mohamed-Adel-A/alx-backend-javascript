08ad7f275b4bf1f13259cab8c1e935bfdc989243
Mohamed Adel <72352419+Mohamed-Adel-A@users.noreply.github.com>
2024-03-21 15:52:32 +0200

    Update 5-http.js

--- 0x05-Node_JS_basic/5-http.js
+++ 0x05-Node_JS_basic/5-http.js
@@ -1,24 +1,42 @@
 const http = require('http');
-const countStudents = require('./3-read_file_async');
+const fs = require('fs');
 
-const app = http.createServer(async (req, res) => {
+const app = http.createServer((req, res) => {
+  res.writeHead(200, { 'Content-Type': 'text/plain' });
+
   if (req.url === '/') {
-    res.writeHead(200, { 'Content-Type': 'text/plain' });
-    res.end('Hello Holberton School!');
+    res.write('Hello Holberton School!\n');
+    res.end();
   } else if (req.url === '/students') {
-    try {
-      await countStudents(process.argv[2]);
-      res.writeHead(200, { 'Content-Type': 'text/plain' });
+    fs.readFile('database.csv', 'utf8', (err, data) => {
+      if (err) {
+        res.write('Cannot load the database');
+        res.end();
+        return;
+      }
+
+      const lines = data.trim().split('\n').slice(1); // Skip first row
+      const students = {};
+      lines.forEach(line => {
+        const [firstname, lastname, age, field] = line.split(',');
+        if (!students[field]) {
+          students[field] = [];
+        }
+        students[field].push(firstname);
+      });
+
+      res.write('This is the list of our students\n');
+      for (const field in students) {
+        if (Object.hasOwnProperty.call(students, field)) {
+          res.write(`Number of students in ${field}: ${students[field].length}. List: ${students[field].join(', ')}\n`);
+        }
+      }
       res.end();
-    } catch (error) {
-      res.writeHead(500, { 'Content-Type': 'text/plain' });
-      res.end(error.message);
-    }
+    });
+  } else {
+    res.statusCode = 404;
+    res.end('Not Found\n');
   }
 });
 
-app.listen(1245, () => {
-  console.log('Server running at http://127.0.0.1:1245/');
-});
-
-module.exports = app;
+app.listen(1245);
