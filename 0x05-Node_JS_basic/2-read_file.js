a9a785cfefbd6498839257f6d66ed729ea5d6aa3
Mohamed Adel <72352419+Mohamed-Adel-A@users.noreply.github.com>
2024-03-21 14:40:40 +0200

    Update 2-read_file.js

--- 0x05-Node_JS_basic/2-read_file.js
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -1,1 +1,26 @@
-#!/usr/bin/node
+const fs = require('fs');
+
+const countStudents = (path) => {
+  try {
+    const data = fs.readFileSync(path, 'utf8').split('\n').filter(line => line !== '');
+    const students = {};
+    data.forEach((line) => {
+      const student = line.split(',');
+      const field = student[3];
+      if (!students[field]) {
+        students[field] = [];
+      }
+      students[field].push(student[0]);
+    });
+    console.log(`Number of students: ${data.length - 1}`);
+    for (const field in students) {
+      if (Object.hasOwnProperty.call(students, field)) {
+        console.log(`Number of students in ${field}: ${students[field].length}. List: ${students[field].join(', ')}`);
+      }
+    }
+  } catch (err) {
+    throw new Error('Cannot load the database');
+  }
+};
+
+module.exports = countStudents;
