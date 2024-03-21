723e19e2d9053bb4e8d9366c4caf0322e0adca92
Mohamed Adel <72352419+Mohamed-Adel-A@users.noreply.github.com>
2024-03-21 14:41:59 +0200

    Update utils.js

--- 0x05-Node_JS_basic/full_server/utils.js
+++ 0x05-Node_JS_basic/full_server/utils.js
@@ -1,1 +1,29 @@
-#!/usr/bin/node
+const fs = require('fs');
+
+const readDatabase = (path) => {
+  return new Promise((resolve, reject) => {
+    fs.readFile(path, 'utf8', (err, data) => {
+      if (err) {
+        reject(new Error('Cannot load the database'));
+      } else {
+        const students = data.split('\n').filter(line => line !== '');
+        const studentsByField = {};
+        students.forEach((student) => {
+          const fields = student.split(',');
+          const field = fields[3];
+          if (!studentsByField[field]) {
+            studentsByField[field] = [];
+          }
+          studentsByField[field].push(fields[0]);
+        });
+        const sortedStudentsByField = {};
+        Object.keys(studentsByField).sort().forEach(key => {
+          sortedStudentsByField[key] = studentsByField[key];
+        });
+        resolve(sortedStudentsByField);
+      }
+    });
+  });
+};
+
+module.exports = readDatabase;
