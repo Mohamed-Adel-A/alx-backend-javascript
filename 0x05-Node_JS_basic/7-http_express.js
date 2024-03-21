809255f8541af9ccf07c80af91c0966bb32732a6
Mohamed Adel <72352419+Mohamed-Adel-A@users.noreply.github.com>
2024-03-22 01:34:05 +0200

    Update 7-http_express.js

--- 0x05-Node_JS_basic/7-http_express.js
+++ 0x05-Node_JS_basic/7-http_express.js
@@ -1,5 +1,34 @@
 const express = require('express');
-const countStudents = require('./3-read_file_async');
+
+function countStudents(path) {
+  return new Promise((resolve, reject) => {
+    fs.readFile(path, 'utf8', (err, data) => {
+      if (err) {
+        reject(new Error('Cannot load the database'));
+      } else {
+        const lines = data.split('\n').filter((line) => line !== '');
+        const students = lines.slice(1);
+        const studentsByField = {};
+        let result;
+        students.forEach((student) => {
+          const fields = student.split(',');
+          const field = fields[3];
+          if (!studentsByField[field]) {
+            studentsByField[field] = [];
+          }
+          studentsByField[field].push(fields[0]);
+        });
+        result = `Number of students: ${students.length}`;
+        for (const field in studentsByField) {
+          if (Object.hasOwnProperty.call(studentsByField, field)) {
+            result += (`\nNumber of students in ${field}: ${studentsByField[field].length}. List: ${studentsByField[field].join(', ')}`);
+          }
+        }
+        resolve(result);
+      }
+    });
+  });
+}
 
 const app = express();
 
@@ -8,12 +37,25 @@ app.get('/', (req, res) => {
 });
 
 app.get('/students', async (req, res) => {
-  try {
-    await countStudents(process.argv[2]);
-    res.send();
-  } catch (error) {
-    res.status(500).send(error.message);
-  }
+  const responseParts = ['This is the list of our students'];
+
+  countStudents(process.argv[2])
+    .then((report) => {
+      responseParts.push(report);
+      const responseText = responseParts.join('\n');
+      res.setHeader('Content-Type', 'text/plain');
+      res.setHeader('Content-Length', responseText.length);
+      res.statusCode = 200;
+      res.write(Buffer.from(responseText));
+    })
+    .catch((err) => {
+      responseParts.push(err instanceof Error ? err.message : err.toString());
+      const responseText = responseParts.join('\n');
+      res.setHeader('Content-Type', 'text/plain');
+      res.setHeader('Content-Length', responseText.length);
+      res.statusCode = 200;
+      res.write(Buffer.from(responseText));
+    });
 });
 
 app.listen(1245, () => {
