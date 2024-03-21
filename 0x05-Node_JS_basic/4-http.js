c687c1043e0ac92bf4b421e41afbdca972d51c45
Mohamed Adel <72352419+Mohamed-Adel-A@users.noreply.github.com>
2024-03-21 14:41:14 +0200

    Update 4-http.js

--- 0x05-Node_JS_basic/4-http.js
+++ 0x05-Node_JS_basic/4-http.js
@@ -1,1 +1,13 @@
-#!/usr/bin/node
+const http = require('http');
+
+const app = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.end('Hello Holberton School!\n');
+});
+
+app.listen(1245, () => {
+  console.log('Server running at http://127.0.0.1:1245/');
+});
+
+module.exports = app;
