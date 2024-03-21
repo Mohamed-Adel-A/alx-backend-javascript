988063d3b025f205cea8ce0686c1d01f142f4370
Mohamed Adel <72352419+Mohamed-Adel-A@users.noreply.github.com>
2024-03-21 14:40:55 +0200

    Update 3-read_file_async.js

--- 0x05-Node_JS_basic/3-read_file_async.js
+++ 0x05-Node_JS_basic/3-read_file_async.js
@@ -1,1 +1,31 @@
-#!/usr/bin/node
+const fs = require('fs');
+
+const countStudents = (path) => {
+  return new Promise((resolve, reject) => {
+    fs.readFile(path, 'utf8', (err, data) => {
+      if (err) {
+        reject(new Error('Cannot load the database'));
+      } else {
+        const students = data.split('\n').filter(line => line !== '');
+        const studentsByField = {};
+        students.forEach((student) => {
+          const fields = student.split(',');
+          const field = fields[3];
+          if (!studentsByField[field]) {
+            studentsByField[field] = [];
+          }
+          studentsByField[field].push(fields[0]);
+        });
+        console.log(`Number of students: ${students.length}`);
+        for (const field in studentsByField) {
+          if (Object.hasOwnProperty.call(studentsByField, field)) {
+            console.log(`Number of students in ${field}: ${studentsByField[field].length}. List: ${studentsByField[field].join(', ')}`);
+          }
+        }
+        resolve();
+      }
+    });
+  });
+};
+
+module.exports = countStudents;
