c41467443bac886679f945765af3b0638f35e3e8
Mohamed Adel <72352419+Mohamed-Adel-A@users.noreply.github.com>
2024-03-22 02:15:30 +0200

    Update 2-calcul_chai.test.js

--- 0x06-unittests_in_js/2-calcul_chai.test.js
+++ 0x06-unittests_in_js/2-calcul_chai.test.js
@@ -1,1 +1,23 @@
-#!/usr/bin/node
+const { expect } = require('chai');
+const calculateNumber = require('./2-calcul_chai');
+
+describe('calculateNumber', () => {
+  describe('type SUM', () => {
+    it('should return the rounded sum of two numbers', () => {
+      expect(calculateNumber('SUM', 1.4, 4.5)).to.equal(6);
+    });
+  });
+
+  describe('type SUBTRACT', () => {
+    it('should return the rounded subtraction of two numbers', () => {
+      expect(calculateNumber('SUBTRACT', 1.4, 4.5)).to.equal(-4);
+    });
+  });
+
+  describe('type DIVIDE', () => {
+    it('should return the rounded division of two numbers', () => {
+      expect(calculateNumber('DIVIDE', 1.4, 4.5)).to.equal(0.2);
+      expect(calculateNumber('DIVIDE', 1.4, 0)).to.equal('Error');
+    });
+  });
+});
