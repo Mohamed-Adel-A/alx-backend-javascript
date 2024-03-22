4ee7e9787d581936ec3a5c938137e53b73137ac1
Mohamed Adel <72352419+Mohamed-Adel-A@users.noreply.github.com>
2024-03-22 02:33:42 +0200

    Update api.test.js

--- 0x06-unittests_in_js/10-api/api.test.js
+++ 0x06-unittests_in_js/10-api/api.test.js
@@ -1,1 +1,25 @@
-#!/usr/bin/node
+const request = require('supertest');
+const server = require('./api');
+
+describe('GET /available_payments', () => {
+  it('responds with payment methods', async () => {
+    const response = await request(server).get('/available_payments');
+    expect(response.status).toBe(200);
+    expect(response.body).toEqual({
+      payment_methods: {
+        credit_cards: true,
+        paypal: false
+      }
+    });
+  });
+});
+
+describe('POST /login', () => {
+  it('responds with welcome message', async () => {
+    const response = await request(server)
+      .post('/login')
+      .send({ userName: 'John' });
+    expect(response.status).toBe(200);
+    expect(response.text).toBe('Welcome John');
+  });
+});
