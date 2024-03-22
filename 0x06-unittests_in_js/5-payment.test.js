36cbf46d30f4dcf5c76f299c1ba6832102f2d8cf
Mohamed Adel <72352419+Mohamed-Adel-A@users.noreply.github.com>
2024-03-22 02:27:02 +0200

    Update 5-payment.test.js

--- 0x06-unittests_in_js/5-payment.test.js
+++ 0x06-unittests_in_js/5-payment.test.js
@@ -1,1 +1,31 @@
-#!/usr/bin/node
+const sinon = require('sinon');
+const assert = require('assert');
+const sendPaymentRequestToApi = require('./5-payment');
+const Utils = require('./utils');
+
+describe('sendPaymentRequestToApi', () => {
+  let calculateNumberStub;
+  let consoleSpy;
+
+  beforeEach(() => {
+    calculateNumberStub = sinon.stub(Utils, 'calculateNumber').returns(120);
+    consoleSpy = sinon.spy(console, 'log');
+  });
+
+  afterEach(() => {
+    calculateNumberStub.restore();
+    consoleSpy.restore();
+  });
+
+  it('should log correct message with total 120', () => {
+    sendPaymentRequestToApi(100, 20);
+    sinon.assert.calledOnce(consoleSpy);
+    sinon.assert.calledWithExactly(consoleSpy, 'The total is: 120');
+  });
+
+  it('should log correct message with total 20', () => {
+    sendPaymentRequestToApi(10, 10);
+    sinon.assert.calledOnce(consoleSpy);
+    sinon.assert.calledWithExactly(consoleSpy, 'The total is: 20');
+  });
+});
