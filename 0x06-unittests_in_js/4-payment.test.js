72eae70e2f26f6337524f7f410e0e6e40badde94
Mohamed Adel <72352419+Mohamed-Adel-A@users.noreply.github.com>
2024-03-22 02:25:38 +0200

    Update 4-payment.test.js

--- 0x06-unittests_in_js/4-payment.test.js
+++ 0x06-unittests_in_js/4-payment.test.js
@@ -1,1 +1,15 @@
-#!/usr/bin/node
+const sinon = require('sinon');
+const assert = require('assert');
+const sendPaymentRequestToApi = require('./4-payment');
+const Utils = require('./utils');
+
+describe('sendPaymentRequestToApi', () => {
+  it('should log correct message with stubbed calculation result', () => {
+    const calculateNumberStub = sinon.stub(Utils, 'calculateNumber').returns(10);
+    const consoleSpy = sinon.spy(console, 'log');
+    sendPaymentRequestToApi(100, 20);
+    sinon.assert.calledWithExactly(consoleSpy, 'The total is: 10');
+    consoleSpy.restore();
+    calculateNumberStub.restore();
+  });
+});
