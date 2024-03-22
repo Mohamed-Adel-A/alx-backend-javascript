877318a43b8353ecc3fab9979ecc3d47d67eec1b
Mohamed Adel <72352419+Mohamed-Adel-A@users.noreply.github.com>
2024-03-22 02:27:31 +0200

    Update 6-payment_token.test.js

--- 0x06-unittests_in_js/6-payment_token.test.js
+++ 0x06-unittests_in_js/6-payment_token.test.js
@@ -1,1 +1,13 @@
-#!/usr/bin/node
+const assert = require('assert');
+const getPaymentTokenFromAPI = require('./6-payment_token');
+
+describe('getPaymentTokenFromAPI', () => {
+  it('should resolve with successful response from API', (done) => {
+    getPaymentTokenFromAPI(true)
+      .then((response) => {
+        assert.deepStrictEqual(response, { data: 'Successful response from the API' });
+        done();
+      })
+      .catch((error) => done(error));
+  });
+});
