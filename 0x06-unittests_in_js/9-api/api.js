2b7382cdefacbaa1062f7466a6be5c4ab85ad19b
Mohamed Adel <72352419+Mohamed-Adel-A@users.noreply.github.com>
2024-03-22 02:32:55 +0200

    Update api.js

--- 0x06-unittests_in_js/9-api/api.js
+++ 0x06-unittests_in_js/9-api/api.js
@@ -1,1 +1,18 @@
-#!/usr/bin/node
+const express = require('express');
+
+const app = express();
+
+app.get('/cart/:id', (req, res) => {
+  const id = req.params.id;
+  if (/^\d+$/.test(id)) {
+    res.status(200).send(`Payment methods for cart ${id}`);
+  } else {
+    res.status(404).send('Invalid cart ID');
+  }
+});
+
+const server = app.listen(7865, () => {
+  console.log('API available on localhost port 7865');
+});
+
+module.exports = server;
