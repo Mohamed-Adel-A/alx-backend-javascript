4b49f4bdb2639e7c23c381e0825aa85e18398837
Mohamed Adel <72352419+Mohamed-Adel-A@users.noreply.github.com>
2024-03-22 02:27:18 +0200

    Update 6-payment_token.js

--- 0x06-unittests_in_js/6-payment_token.js
+++ 0x06-unittests_in_js/6-payment_token.js
@@ -1,1 +1,11 @@
-#!/usr/bin/node
+const getPaymentTokenFromAPI = (success) => {
+  return new Promise((resolve, reject) => {
+    if (success) {
+      resolve({ data: 'Successful response from the API' });
+    } else {
+      reject(new Error('Unsuccessful response from the API'));
+    }
+  });
+};
+
+module.exports = getPaymentTokenFromAPI;
