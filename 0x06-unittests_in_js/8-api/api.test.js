9b4bc8c20041850cf77f40ac19089287868c5ff8
Mohamed Adel <72352419+Mohamed-Adel-A@users.noreply.github.com>
2024-03-22 02:31:49 +0200

    Update api.test.js

--- 0x06-unittests_in_js/8-api/api.test.js
+++ 0x06-unittests_in_js/8-api/api.test.js
@@ -1,1 +1,10 @@
-#!/usr/bin/node
+const request = require('supertest');
+const server = require('./app');
+
+describe('GET /', () => {
+  it('responds with welcome message', async () => {
+    const response = await request(server).get('/');
+    expect(response.status).toBe(200);
+    expect(response.text).toBe('Welcome to the payment system');
+  });
+});
