15f37da7e54a8471d511f545771710df1cd6d9f1
Mohamed Adel <72352419+Mohamed-Adel-A@users.noreply.github.com>
2024-03-22 02:17:04 +0200

    Update 3-payment.js

--- 0x06-unittests_in_js/3-payment.js
+++ 0x06-unittests_in_js/3-payment.js
@@ -1,1 +1,8 @@
-#!/usr/bin/node
+const Utils = require('./utils');
+
+const sendPaymentRequestToApi = (totalAmount, totalShipping) => {
+  const sum = Utils.calculateNumber(totalAmount, totalShipping);
+  console.log(`The total is: ${sum}`);
+};
+
+module.exports = sendPaymentRequestToApi;
