074c534ef126a71267ea7480a4e5e871bdba3885
Mohamed Adel <72352419+Mohamed-Adel-A@users.noreply.github.com>
2024-03-22 02:33:32 +0200

    Update api.js

--- 0x06-unittests_in_js/10-api/api.js
+++ 0x06-unittests_in_js/10-api/api.js
@@ -1,1 +1,24 @@
-#!/usr/bin/node
+const express = require('express');
+
+const app = express();
+
+app.get('/available_payments', (req, res) => {
+  const paymentMethods = {
+    payment_methods: {
+      credit_cards: true,
+      paypal: false
+    }
+  };
+  res.status(200).json(paymentMethods);
+});
+
+app.post('/login', (req, res) => {
+  const { userName } = req.body;
+  res.status(200).send(`Welcome ${userName}`);
+});
+
+const server = app.listen(7865, () => {
+  console.log('API available on localhost port 7865');
+});
+
+module.exports = server;
