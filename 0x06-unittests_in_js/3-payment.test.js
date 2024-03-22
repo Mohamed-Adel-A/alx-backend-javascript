2a2b4f7761a9c9342048aebbbdf84a845eb2b993
Mohamed Adel <72352419+Mohamed-Adel-A@users.noreply.github.com>
2024-03-22 02:17:18 +0200

    Update 3-payment.test.js

--- 0x06-unittests_in_js/3-payment.test.js
+++ 0x06-unittests_in_js/3-payment.test.js
@@ -1,1 +1,12 @@
-#!/usr/bin/node
+const sinon = require('sinon');
+const sendPaymentRequestToApi = require('./3-payment');
+const Utils = require('./utils');
+
+describe('sendPaymentRequestToApi', () => {
+  it('should call Utils.calculateNumber with correct arguments', () => {
+    const calculateNumberSpy = sinon.spy(Utils, 'calculateNumber');
+    sendPaymentRequestToApi(100, 20);
+    sinon.assert.calledWithExactly(calculateNumberSpy, 100, 20);
+    calculateNumberSpy.restore();
+  });
+});
