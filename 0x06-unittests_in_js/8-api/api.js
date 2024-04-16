528cc6da1f6820d2992259eebe8f4f3ab6b31303
Mohamed Adel <72352419+Mohamed-Adel-A@users.noreply.github.com>
2024-04-16 12:18:05 +0200

    Update api.js

--- 0x06-unittests_in_js/8-api/api.js
+++ 0x06-unittests_in_js/8-api/api.js
@@ -1,13 +1,12 @@
 const express = require('express');
 const app = express();
-const PORT = 7865;
 
-app.get('/', (_, res) => {
-  res.send('Welcome to the payment system');
+app.get('/', function (request, response) { // Change route to '/'
+  response.send('Welcome to the payment system');
 });
 
-app.listen(PORT, () => {
-  console.log(`API available on localhost port ${PORT}`);
+app.listen(7865, function () {
+  console.log('API available on localhost port 7865');
 });
 
 module.exports = app;
