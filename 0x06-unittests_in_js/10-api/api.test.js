db74563d3c5e377cb38837ba65b6b71114f92bf2
Mohamed Adel <72352419+Mohamed-Adel-A@users.noreply.github.com>
2024-04-16 12:45:26 +0200

    Update api.test.js

--- 0x06-unittests_in_js/10-api/api.test.js
+++ 0x06-unittests_in_js/10-api/api.test.js
@@ -23,3 +23,37 @@ describe('POST /login', () => {
     expect(response.text).toBe('Welcome John');
   });
 });
+
+describe('Cart page', function() {
+  it('should return status code 200 when :id is a number', function(done) {
+    request.get('http://localhost:7865/cart/123', function(error, response, body) {
+      expect(response.statusCode).to.equal(200);
+      done();
+    });
+  });
+
+  it('should return status code 404 when :id is NOT a number', function(done) {
+    request.get('http://localhost:7865/cart/hello', function(error, response, body) {
+      expect(response.statusCode).to.equal(404);
+      done();
+    });
+  });
+
+  it('Test for GET / returns “Welcome to the payment system” exists', function(done) {
+    request.get('http://localhost:7865/', function(error, response, body) {
+      expect(response.statusCode).to.equal(200);
+      expect(body).to.equal('Welcome to the payment system');
+      done();
+    });
+  });
+
+  it('should return status code 200 and correct message when :id is a number', function(done) {
+    request.get('http://localhost:7865/cart/123', function(error, response, body) {
+      expect(response.statusCode).to.equal(200);
+      expect(body).to.equal('Payment methods for cart 123');
+      done();
+    });
+  });
+
+  // Add other tests if needed
+}); 
