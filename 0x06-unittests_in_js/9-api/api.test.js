b833ec79c7e483c0d32fa34840f079f49c764b1e
Mohamed Adel <72352419+Mohamed-Adel-A@users.noreply.github.com>
2024-04-16 12:39:26 +0200

    Update api.test.js

--- 0x06-unittests_in_js/9-api/api.test.js
+++ 0x06-unittests_in_js/9-api/api.test.js
@@ -25,13 +25,5 @@ describe('Cart page', function() {
     });
   });
 
-  it('should return status code 200 and correct message when :id is a number', function(done) {
-    request.get('http://localhost:7865/cart/123', function(error, response, body) {
-      expect(response.statusCode).to.equal(200);
-      expect(body).to.equal('Payment methods for cart 123');
-      done();
-    });
-  });
-
   // Add other tests if needed
 }); 
