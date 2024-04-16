9d359c32d8f3bae5ec0a1f9fd524ff8e1ce9d5e4
Mohamed Adel <72352419+Mohamed-Adel-A@users.noreply.github.com>
2024-04-16 13:00:40 +0200

    Update api.js

--- 0x06-unittests_in_js/10-api/api.js
+++ 0x06-unittests_in_js/10-api/api.js
@@ -1,24 +1,39 @@
 const express = require('express');
-
+const bodyParser = require('body-parser');
 const app = express();
 
-app.get('/available_payments', (req, res) => {
+app.use(bodyParser.json());
+
+// Route for GET /
+app.get('/', function (req, res) {
+  res.send('Welcome to the payment system');
+});
+
+// Route for GET /cart/:id
+app.get('/cart/:id(\\d+)', function (req, res) { // Regex to accept only numbers for :id
+  const cartId = req.params.id;
+  res.send(`Payment methods for cart ${cartId}`);
+});
+
+// Route for GET /available_payments
+app.get('/available_payments', function(req, res) {
   const paymentMethods = {
     payment_methods: {
       credit_cards: true,
       paypal: false
     }
   };
-  res.status(200).json(paymentMethods);
+  res.json(paymentMethods);
 });
 
-app.post('/login', (req, res) => {
-  const { userName } = req.body;
-  res.status(200).send(`Welcome ${userName}`);
+// Route for POST /login
+app.post('/login', function(req, res) {
+  const username = req.body.userName;
+  res.send(`Welcome ${username}`);
 });
 
-const server = app.listen(7865, () => {
+app.listen(7865, function () {
   console.log('API available on localhost port 7865');
 });
 
-module.exports = server;
+module.exports = app;
