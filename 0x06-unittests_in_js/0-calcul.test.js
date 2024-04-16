161f9e1ece3938e78abbf8e55a5dbb363502c78f
Mohamed Adel <72352419+Mohamed-Adel-A@users.noreply.github.com>
2024-04-16 11:55:38 +0200

    Update 0-calcul.test.js

--- 0x06-unittests_in_js/0-calcul.test.js
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -5,8 +5,14 @@ describe('calculateNumber', function() {
   it('should return the sum of two rounded numbers', function() {
     assert.strictEqual(calculateNumber(1, 3), 4);
     assert.strictEqual(calculateNumber(1, 3.7), 5);
-    assert.strictEqual(calculateNumber(3.7, 1), 5);
     assert.strictEqual(calculateNumber(1.2, 3.7), 5);
     assert.strictEqual(calculateNumber(1.5, 3.7), 6);
+  });
+
+  it('should return the sum of the first number rounded and the second number rounded', function() {
+    assert.strictEqual(calculateNumber(1.3, 3), 4);
+    assert.strictEqual(calculateNumber(1.7, 3), 5);
+    assert.strictEqual(calculateNumber(1.7, 3.2), 5);
+    assert.strictEqual(calculateNumber(1.7, 3.5), 6);
   });
 });
