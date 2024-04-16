ac26070ad249b27848b18de7c304d1ac1223d63c
Mohamed Adel <72352419+Mohamed-Adel-A@users.noreply.github.com>
2024-04-16 12:08:59 +0200

    Task 8

--- 0x06-unittests_in_js/8-api/api.js
+++ 0x06-unittests_in_js/8-api/api.js
@@ -3,11 +3,11 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.status(200).send('Welcome to the payment system');
+  res.send('Welcome to the payment system');
 });
 
-const server = app.listen(7865, () => {
+app.listen(7865, () => {
   console.log('API available on localhost port 7865');
 });
 
-module.exports = server;
+module.exports = app;
